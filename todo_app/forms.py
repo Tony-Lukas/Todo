a6cbf9479aa8e416f1aa57472494a68b295828ee
Tony-Lukas <115721512+Tony-Lukas@users.noreply.github.com>
2024-10-26 20:42:55 +0700

    Form title

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -3,19 +3,29 @@ from .models import TodoItem,TodoGroup
 from datetime import datetime, timezone
 
 class GroupForm(forms.ModelForm):
+    name = forms.CharField(label="",
+                           widget=(forms.widgets.TextInput(attrs={
+                               'placeholder':'What Your new projects',
+                               'class':'form-control',
+                           })))
     class Meta:
         model = TodoGroup
         fields = '__all__'
 
 class DetailForm(forms.ModelForm):
-    cleaned_data = datetime.now(timezone.utc)
+    #cleaned_data = datetime.now(timezone.utc)
+    group = forms.ModelChoiceField(TodoGroup.objects.all(),
+                                   label='',required=True,
+                                    widget=(forms.widgets.Select(attrs={
+                                        'class':'form-control',
+                                    })))
     task = forms.CharField(label='',required=True, 
                            widget=(forms.widgets.TextInput(attrs={
                                'placeholder':'What is Next?',
                                'class':'form-control',
                            })))
     details = forms.CharField(label='',required=False, 
-                           widget=(forms.widgets.Textarea(attrs={
+                           widget=(forms.widgets.TextInput(attrs={
                                'placeholder':'Please Tell me detail?',
                                'class':'form-control',
                                'rows':'5',
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.main, name='main'),
-    path('item_main/<int:group_id>',views.item_main,name='item_main'),
+    path('add_group/',views.add_group,name='add_group'),
     path('delete_group/<int:group_id>',views.delete_group,name='delete_group'),
     path('update_group/<int:group_id>',views.update_group, name='update_group'),
-
+    
+    path('item_main/<int:group_id>',views.item_main,name='item_main'),
     path('add_detail/<int:pk>',views.add_detail,name='add_detail'),
     path('done_task/<int:pk>',views.done_task,name='done_task'),
     path('delete_task/<int:pk>',views.delete_task,name='delete_task'),
